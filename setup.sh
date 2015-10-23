ea07d8c710804ec2f80e10b39b5349d903281fd8
Noah Weinthal <noah@flipper.local>
2015-10-23 15:16:21 -0400

    Update setup script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@ sudo easy_install pip;
 source ./venv/bin/activate;
 pip install -r requirements.txt;
 cd src/septa_challenge;
-python manage.py collectstatic;
+yes "yes" | python manage.py collectstatic;
 python manage.py migrate;
 python manage.py runserver;
