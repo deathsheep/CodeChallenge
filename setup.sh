593cd2f498003f995d4c72ad2fa07e18be211545
Noah Weinthal <noah@flipper.local>
2015-10-23 15:47:19 -0400

    Moar setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+#!venv/bin python
 sudo easy_install pip;
+sudo chown -R $USER:$USER .
 source ./venv/bin/activate;
+which pip
+which python
 pip install -r requirements.txt;
 cd src/septa_challenge;
 yes "yes" | python manage.py collectstatic;
